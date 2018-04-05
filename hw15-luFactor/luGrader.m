33963b566d3b3e222ab12e717d8a2edbd324dcb6
Nate <noverton@linuxe1.engr.colostate.edu>
2018-04-05 14:37:30 -0600

    Catching some possible errors with the LU function. No way to check for wrong output order yet.

--- hw15-luFactor/luGrader.m
+++ hw15-luFactor/luGrader.m
@@ -37,8 +37,8 @@ catch
         try
         evalc('[L] = luFactor(A)');
         catch
-            notes = ['Function fails to run or has no output, -80.', ' '];
-            points = points - 80;
+            notes = ['Function fails to run or has no output, -100.', ' '];
+            points = points - 100;
             return
         end
         notes = [notes,'Pivot matrix missing.', ' '];
@@ -50,10 +50,24 @@ end
 passed = [0, 0, 0];
 
 fprintf('Second Test: A 3x3 matrix\n')
+% check matrix size
+if any(size(U) ~= size(Ue)) || any(size(L) ~= size(Le)) || any(size(P) ~= size(Pe))
+    notes = [notes, 'Function does not return matricies of correct size, -80.', ' '];
+    points = points - 80;
+    return
+end
+% make sure the return order is correct
+if istriu(L) && istril(U)
+    notes = [notes, 'Return order of L and U is backwards, -10.', ' '];
+    points = points -10;
+    temp = U;
+    U = L;
+    L = temp;
+end
 % examine U
 if ~istriu(U) 
-    notes = [notes, 'U is not upper triangular, -20.',  ' '];
-    points = points - 20;
+    notes = [notes, 'U is not upper triangular, -16.',  ' '];
+    points = points - 16;
 elseif norm(Ue - U) > tol
     notes = [notes, 'U is incorrect, -10.', ' '];
     points = points - 10;
@@ -63,8 +77,8 @@ else
 end
 % examine L
 if ~istril(L) 
-    notes = [notes, 'L is not lower triangular, -20.',  ' '];
-    points = points - 20;
+    notes = [notes, 'L is not lower triangular, -16.',  ' '];
+    points = points - 16;
 elseif norm(Le - L) > tol
     notes = [notes, 'L is incorrect, -10.', ' '];
     points = points - 10;
@@ -74,8 +88,8 @@ else
 end
 % examine P
 if norm(P) ~= 1
-    notes = [notes, 'P is not a valid permutation, -20.',  ' '];
-    points = points - 20;
+    notes = [notes, 'P is not a valid permutation, -16.',  ' '];
+    points = points - 16;
 elseif norm(Pe - P) > tol
     notes = [notes, 'P is incorrect, -10.', ' '];
     points = points - 10;
@@ -87,8 +101,8 @@ end
 if (norm(P*A - L*U) < tol) && ~(passed(1) && passed(2))
     notes = [notes, 'L*U=P*A is valid,', ' '];
     if(trace(L) ~= 3)
-        notes = [notes, 'but not in standard form, +15 to compensate.', ' '];
-        points = points + 15;
+        notes = [notes, 'but not in standard form, +10 to compensate.', ' '];
+        points = points + 10;
     elseif ~passed(3)
         notes = [notes, 'but not pivoted correctly, +20 to compensate.', ' '];
         points = points + 20;
@@ -109,32 +123,44 @@ A = magic(7) - magic(7)';
 try
     evalc('[L, U, P] = luFactor(A)');
 catch
-    notes = [notes, 'Factorization of hard matrix failed, -30.', ' '];
-    points = points - 30;
+    notes = [notes, 'Factorization of hard matrix failed, -21.', ' '];
+    points = points - 21;
     return
 end
 passed = [0, 0, 0];
 fprintf('Third Test: A "hard" matrix\n')
+% check matrix size
+if any(size(U) ~= size(Ue)) || any(size(L) ~= size(Le)) || any(size(P) ~= size(Pe))
+    notes = [notes, 'Function only works for fixed size, -21.', ' '];
+    points = points - 21;
+    return
+end
+% make sure the return order is correct
+if istriu(L) && istril(U)
+    temp = U;
+    U = L;
+    L = temp;
+end
 % examine U
-if norm(Ue-U) > tol
-    notes = [notes, 'U is incorrect for hard matrix, -10.', ' '];
-    points = points - 10;
+if norm(Ue-U) > tol || ~isreal(norm(Ue-U))
+    notes = [notes, 'U is incorrect for hard matrix, -7.', ' '];
+    points = points - 7;
 else
     fprintf('  Correct U\n')
     passed(1) = 1;
 end
 % examine L
-if norm(Le-L) > tol
-    notes = [notes, 'L is incorrect for hard matrix, -10.', ' '];
-    points = points - 10;
+if norm(Le-L) > tol || ~isreal(norm(Le-L))
+    notes = [notes, 'L is incorrect for hard matrix, -7.', ' '];
+    points = points - 7;
 else
     fprintf('  Correct L\n')
     passed(2) = 1;
 end
 % examine P
 if norm(Pe-P) > tol
-    notes = [notes, 'P is incorrect for hard matrix, -10.', ' '];
-    points = points - 10;
+    notes = [notes, 'P is incorrect for hard matrix, -7.', ' '];
+    points = points - 7;
 else
     fprintf('  Correct P\n')
     passed(3) = 1;
@@ -143,14 +169,11 @@ end
 if (norm(P*A - L*U) < tol) && ~(passed(1) && passed(2))
     notes = [notes, 'L*U=P*A is valid,', ' '];
     if(trace(L) ~= 7)
-        notes = [notes, 'but not in standard form, +15 to compensate.', ' '];
-        points = points + 15;
-    elseif ~passed(3)
-        notes = [notes, 'but not pivoted correctly, +20 to compensate.', ' '];
-        points = points + 20;
+        notes = [notes, 'but not in standard form, +7 to compensate.', ' '];
+        points = points + 7;
     else
-        notes = [notes, 'but factored differently, +10 to compensate.', ' '];
-        points = points + 10;
+        notes = [notes, 'but factored differently, +14 to compensate.', ' '];
+        points = points + 14;
     end
 end
 
